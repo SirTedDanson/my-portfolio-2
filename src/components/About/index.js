2b079d88500a005a30f62a3182cfaa95b7074b60
sirteddanson <knowledgeMED@gmail.com>
2022-03-25 19:35:36 -0400

    About section and CSS added

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about-me" className="section-row">
+      <div className="section-title">
+        <h2> About Me </h2>
+      </div>
+      <article className="about-me">
+        <p>
+          I am an aspiring developer and content creator. I enjoy science,
+          computers, and philosophy. I have a passion for understanding the
+          world, pursuing new perspectives and finding solutions to problems.
+          Working with computers has been a passion of mine for a very long
+          time, I began building computers at age 10. I am currently new to web
+          development, professionally I have experience in quality assurance,
+          process auditing, and hospitality. In my leisure I play video games
+          and enjoy traveling.
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default About;
